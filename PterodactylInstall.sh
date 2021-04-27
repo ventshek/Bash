10394586262f7159a00339565ec9794b36265d69
ventshek <82648360+ventshek@users.noreply.github.com>
2021-04-27 10:22:13 +0200

    Update and rename 101.sh to PterodactylInstall.sh

---
+++ PterodactylInstall.sh
@@ -0,0 +1,32 @@
+#Complete installation
+#Prerequisites
+
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
+sudo apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+sudo add-apt-repository -y ppa:chris-lea/redis-server
+sudo apt install -y net-tools
+sudo curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
+sudo apt -y install php8.0 php8.0-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo mkdir -p /var/www/pterodactyl
+cd /var/www/pterodactyl
+sudo curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+sudo tar -xzvf panel.tar.gz
+sudo chmod -R 755 storage/* bootstrap/cache/
+sudo cp .env.example .env
+yes | sudo composer install --no-dev --optimize-autoloader
+sudo php artisan key:generate --force
+intip=$(hostname -I)
+extip=$(curl ipinfo.io/ip)
+sudo wget https://github.com/ventshek/Bash/raw/main/proc.exp
+sudo chmod +x proc.exp
+sudo ./proc.exp "$intip"
+sudo rm ./proc.exp
+cd
+sudo wget https://github.com/ventshek/Bash/raw/main/redis.conf
+sudo cp -f ./redis.conf /etc/redis/redis.conf
+sudo rm ./redis.conf
+sudo chown -R redis:redis /etc/redis/redis.conf
+
